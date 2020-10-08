e76a8495ea981ed7c4c3faf87a66eedf0a79195f
Abraham Diop <abraham.diop@daviestech.org>
2020-10-08 22:12:18 +0000

    Done

---
+++ question1.py
@@ -0,0 +1,4 @@
+x = int(input("Enter an integer: "))
+print(x + 2)
+print(x * 2)
+print(x / 2)
---
+++ question2.py
@@ -0,0 +1,8 @@
+a = float(input("Enter a decimal: "))
+
+b = float(input("Enter another decimal: "))
+
+c = float(input("Enter a third decimal: "))
+
+
+print("Sum is " + str(a + b + c))
